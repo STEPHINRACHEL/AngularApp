7acff8f18cb7583c236f1ff476c15a1f6b7d7e26
Stephin Thomas <stethoma@redhat.com>
2019-07-28 00:13:39 +0200

    users-list created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/users.ts
@@ -0,0 +1,44 @@
+export const employees = [
+    {
+      No: 1,
+      FirstName: 'Mark',
+      LastName: 'Twain',
+      email: 'marktwain@abc.com',
+      age: 35
+    },
+    {
+      No: 2,
+      FirstName: 'John',
+      LastName: 'Smith',
+      email: 'marktwain@abc.com',
+      age: 45
+    },
+    {
+      No: 3,
+      FirstName: 'Agatha',
+      LastName: 'Christie',
+      email: 'marktwain@abc.com',
+      age: 27
+    },
+    {
+      No: 4,
+      FirstName: 'Janne',
+      LastName: 'Jim',
+      email: 'marktwain@abc.com',
+      age: 40
+    },
+    {
+      No: 5,
+      FirstName: 'Tim',
+      LastName: 'Corbet',
+      email: 'timcorbet@abc.com',
+      age: 31
+    },
+    {
+      No: 6,
+      FirstName: 'Alex',
+      LastName: 'Webb',
+      email: 'alexwebb@abc.com',
+      age: 48
+    }
+  ];
